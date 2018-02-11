767878fe9bd5e67198af5aefc5dd2410ae4c3ca0
Linda Foo <linda.foo.dev@gmail.com>
2018-02-10 16:38:19 -0800

    starting portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -4,19 +4,19 @@ import '../css/portfolio.css';
 
 export default function Portfolio() {
     return (
-       <div>
-          <section>
+       <div id="portfolio">
+          <section className="portfolio">
             <h2>
               Portfolio
             </h2>
-            <div>
-              <div>
+            <div className="portfolio-container">
+              <div className="project-one-container">
                 <h5>Project 1</h5>
                 <p> Text </p>
                 <h6> Skills </h6>
                 <p> Skills Text </p>
               </div>
-              <div>
+              <div className="project-two-container">
                 <h5>Project 2</h5>
                 <p> Text 2</p>
                 <h6> Skills 2</h6>
