a9d9d3f4adab7da0fbe83f3667434ac7fc5c31c7
Linda Foo <linda.foo.dev@gmail.com>
2018-02-19 17:46:05 -0800

    renamed image file and setting up nav bar including react sticky library

--- src/components/home.js
+++ src/components/home.js
@@ -12,47 +12,72 @@ export default class Home extends React.Component {
     render(){
         return (
             <div className='container'>
-                <div className='row'>
-                    <div className='col-lg-3 col-sm-6'>
-                        <div className="card hovercard" >
-                            <div className="cardheader">
-                                
-                            </div>
-                            <div className="avatar">
-                                <img src={require('./LinkedInImage.jpg')} alt={"linda foo profile"}/>
-                            </div>
-                            <div className="info">
-                                <div className="title">
-                                    Linda Foo
-                                </div>
-                                <div className="desc">Developer</div>
-                            </div>
-                            <div>
-                                <p>Add nav bar here</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-lg-3 col-sm-6'>
-                        <div className="card contentcard" >
-                            <div className="smooth-spacer">
-                                
-                            </div>
-                            <div className="info">
-                                <div className="title">
-                                    Question & Answer
-                                </div>
-                                <div className="desc">Why are you here?</div>
-                            </div>
-                            <div className="detail">
-                                <div className="content">
-                                    <p>Descriptiong</p>
+                <header id='top'>
+                        <div className='row'>
+                            <div className='col-lg-3 col-sm-6'>
+                                <div className="card hovercard" >
+                                    <div className="cardheader">
+                                        
+                                    </div>
+                                    <div className="avatar">
+                                        <img src={require('./avatar-linda.jpg')} alt={"linda foo profile"}/>
+                                    </div>
+                                    <div className="info">
+                                        <div className="title">
+                                            Linda Foo
+                                        </div>
+                                        <div className="desc">Developer</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                </header>
+                <nav className='topNav'>
+                    <ul>
+                        <li className="qanda">
+                            <a className="qanda" href="#qanda">
+                                Q&A
+                            </a>
+                        </li>
+                        <li className="portfolio">
+                            <a className="portfolio" href="#portfolio">
+                                PORTFOLIO
+                            </a>
+                        </li>
+                        <li className="resume">
+                            <a className="resume" href="#resume">
+                                RESUME
+                            </a>
+                        </li>
+                        <li className="contact">
+                            <a className="contact" href="#contact">
+                                CONTACT
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <section id="qanda">           
+                    <div className='row'>
+                        <div className='col-lg-3 col-sm-6'>
+                            <div className="card contentcard" >
+                                <div className="smooth-spacer">
+                                    
+                                </div>
+                                <div className="info">
+                                    <div className="title">
+                                        Question & Answer
+                                    </div>
+                                    <div className="desc">Why are you here?</div>
+                                </div>
+                                <div className="detail">
+                                    <div className="content">
+                                        <p>Descriptiong</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </section>
             </div>
         );
     }
