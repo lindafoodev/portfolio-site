7b10dd822af9d35d4c7015bedd55b74c59a56ac1
Linda Foo <linda.foo.dev@gmail.com>
2018-02-23 17:37:52 -0800

    updated content of Q&A section, minor update to project descriptions

--- src/components/home.js
+++ src/components/home.js
@@ -114,8 +114,8 @@ export default class Home extends React.Component {
                                         </div>
                                         <div className="detail">
                                             <div className="content">
-                                                <p className="qp first-paragraph">I am a former accountant turned developer who is known for my ability to effectively decipher information and efficiently utilize resources available to me while maintaining a professional and dedicated work ethic. I have a proven track record of continually taking on more challenges in order to proactively drive my development.</p><br/>
-                                                <p className="qp second-paragraph">I graduated from The University of Texas at Austin with a BBA and a MPA. I am a licensed CPA. My professional experiences include performing operational reviews for JPMorgan Chase & Co. and external financial audits for Grant Thornton LLP and PricewaterhouseCoopers LLP. I have worked on a variety of clients ranging in industries and functions developing reliable interpersonal and critical thinking skills.</p>
+                                                <p className="qp first-paragraph">I am a former accountant turned developer who is known for my ability to effectively decipher information and efficiently utilize resources available to me to drive performance. I have a proven track record of continuosly taking on challenges to further my personal development.</p><br/>
+                                                <p className="qp second-paragraph">I graduated from The University of Texas at Austin with a Bachelor of Business Administration and a Master in Professional Accounting. I am a licensed CPA. My professional experiences include managing operational reviews for JPMorgan Chase & Co. and external financial audits for PricewaterhouseCoopers LLP (PwC) and Grant Thornton LLP. I have worked on a variety of projects in various industries including Financial Services, Manufacturing, Aerospace, Restaurant, and Oil & Gas.</p>
                                             </div>
                                         </div>
                                     </div>
@@ -131,14 +131,11 @@ export default class Home extends React.Component {
                                             
                                         </div>
                                         <div className="info">
-                                            <div className="desc">Why programming?</div>
+                                            <div className="desc">What are you passionate about?</div>
                                         </div>
                                         <div className="detail">
                                             <div className="content">
-                                                <p className="qp first-paragraph">I enjoy programming because of the emphasis on creating through logic and structure.</p><br/>
-                                                <p className="qp second-paragraph"> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p><br/>
-                                                <p className="qp third-paragraph"> Lore </p><br/>
-                                                <p className="qp fourth-paragraph">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p><br/>
+                                                <p className="qp first-paragraph">I am passionate about making a positive impact in my community and growing through new experiences. I appreciate challenges and I continuously seek out opportunities for growth whether it’s surrounding myself with talented people or exposing myself to other cultures through travel. I strive to be a better version of myself and an active contributor to society.</p>
                                             </div>
                                         </div>
                                     </div>
@@ -153,22 +150,24 @@ export default class Home extends React.Component {
                                             
                                         </div>
                                         <div className="info">
-                                            <div className="desc">What motivates you?</div>
+                                            <div className="desc">Why programming?</div>
                                         </div>
                                         <div className="detail">
                                             <div className="content">
-                                                <p className="qp first-paragraph"> Lorem ipsum commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                                                <p className="qp first-paragraph">In 2017, I was selected as a Lesbians Who Tech Edie Windsor Coding Scholar, which kick-started my journey as a developer.</p><br/>
+                                                <p className="qp second-paragraph">Being a developer has provided me with the ability to leverage my previous professional experiences and business acumen to build real-life solutions for everyday problems to improve people’s quality of life.</p><br/>
+                                                <p className="qp third-paragraph">Programming is fun to me because I enjoy the process of problem solving and I love creating through logic and structure. I appreciate the ability to work independently while also being a part of a community full of encouraging and knowledgeable people willing to collaborate and share their experiences. </p>
                                             </div>
                                         </div>
                                         <div className="info">
-                                            <div className="desc">What is your dream job?</div>
+                                            <div className="desc">How does your past experience make you a good developer?</div>
                                         </div>
                                         <div className="detail">
                                             <div className="content">
-                                                <p className="qp first-paragraph">Culture of collaboration and innovation. Ability to grow and contribute.will leverage my previous professional experiences and nurture my acumen for business and technology.</p><br/>
-                                                <p className="qp second-paragraph">obtain a master in CS from credible part time program.be in a room full of smarter people, share and collaborate. Leverage my professional experience and apply the same skills for success (problem-solve, decipher info, be challenge, opportunity through organization)</p>
-                                                <p className="qp second-paragraph">obtain a master in CS from credible part time program.be in a room full of smarter people, share and collaborate. Leverage my professional experience and apply the same skills for success (problem-solve, decipher info, be challenge, opportunity through organization)</p>
-                                                <p className="qp second-paragraph">obtain a master in CS from credible part time program.be in a room full of smarter people, share and collaborate. Leverage my professional experience and apply the same skills for success (problem-solve, decipher info, be challenge, opportunity through organization)</p>
+                                                <p className="qp first-paragraph">Throughout my five years of professional experiences, I developed transferable skills that make me a productive developer.</p><br/>
+                                                <p className="qp second-paragraph">I developed reliable interpersonal skills. One time, when I was managing a financial audit engagement, my Senior Manager approached me and asked for help. She needed to track down documents that were critical to completing the audit. The deadline was the next day. Typically, the turnaround time was 3 to 5-days to get support. Since I had worked with the client for the past few months and had intimate knowledge of the business, I quickly identified the key person to talk to. I took the initiative to gather the support from him. Due to the strong strategic relationship I had built in the past, I was able to meet him that day despite his busy schedule. I conveyed the importance of obtaining the documents and convinced him to provide the support immediately. As a result, he put my request as his first priority and sent the documents the same day.</p><br/>
+                                                <p className="qp third-paragraph">I built strong analytical skills. I routinely performed analysis on financial data to identify trends and discrepancies. In one instance, my analysis resulted in identifying a $1.5 million misstatement in a company’s revenue recognition. My analysis prevented them from incurring future costs and losing investors if the misstatement was not fixed. </p><br/>
+                                                <p className="qp fourth-paragraph">I gained strategic vision. My previous positions allowed me to work with people across functions and levels, from staff to Executives. As a result, I have first-hand experience on how big and small companies operate and how I can align the company’s strategies with day-to-day operations. For example, one client was looking for ways to cut operational costs. While working with them, I identified inefficiencies in their recordkeeping that was also impacting our ability to perform a timely audit. I created standardized accounting templates to replace their manual process, resulting in a 104% increase in client productivity and securing our engagement with the client the following years.</p><br/>
                                             </div>
                                         </div>
                                     </div>
@@ -205,7 +204,7 @@ export default class Home extends React.Component {
                                                 </div>
                                                 <h6> Skills</h6>
                                                 <div className="project-one-skills">
-                                                    <p className="pp">Javascript ES6, jQuery, Nodejs, Express, Bcryptjs, jsonwebtoken, Passport, Mongodb, Mongoose, Heroku, MLab, Travis CI, Github, Mocha, Chai </p>
+                                                    <p className="pp">Javascript ES6, jQuery, Nodejs, Express, Bcryptjs, jsonwebtoken, Passport, Mongodb, Mongoose, Heroku, mLab, Travis CI, Github, Mocha, Chai </p>
                                                 </div>
                                             </div>
                                             </div>
@@ -222,11 +221,11 @@ export default class Home extends React.Component {
                                                         </div>
                                                     </div>
                                                 <div className="project-two-description">
-                                                <p className="pp">Zhip is a utility application to send and receive IOUs between individuals reliably and securely over the web. </p>
+                                                <p className="pp">Full-stack MERN utility web application to send and receive IOUs between individuals reliably and securely using a generated URL link.</p>
                                                 </div>
                                                 <h6> Skills</h6>
                                                 <div className="project-two-skills">
-                                                    <p className="pp">Javascript ES6, Nodejs, Express, React, Redux, Bcryptjs, jsonwebtoken, Passport, MySQL, Heroku, MLab, Travis CI, Github, Mocha, Chai</p>
+                                                    <p className="pp">Javascript ES6, Nodejs, Express, React, Redux, Mongodb, Mongoose, Heroku, mLab, Travis CI, Github, Mocha, Chai</p>
                                                 </div>
                                             </div>                                           
                                         </div>
