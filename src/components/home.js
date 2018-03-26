b4b9b2c1ef062514602ec2fe42d872a719f00503
Linda Foo <linda.foo.dev@gmail.com>
2018-03-25 18:27:46 -0700

    initial setup

--- src/components/home.js
+++ src/components/home.js
@@ -2,8 +2,8 @@
 
 import React from 'react';
 
-//import Scroll from './scroll';
-//import BoundingRect from './bounding-rect';
+import Scroll from './scroll';
+import BoundingRect from './bounding-rect';
 //import { StickyContainer, Sticky } from 'react-sticky';
 
 // import LandingPage from './landing-page';
@@ -18,11 +18,88 @@ import React from 'react';
                 <Resume />
                 <Contact /> */
 
+const containerHeight = '100px';
+
+const styles = {
+    sticky: { 'width': '100%', position: 'fixed', top: '0', right: '0', 'backgroundColor': '#AEC5CB', border: '1px solid #EDEDED'},
+    container: { width: '100%', height: containerHeight, overflow: 'hidden', position: 'relative'},
+    boundingRect: { width: '100%', height: containerHeight },
+};
+
 export default class Home extends React.Component {
 
     render(){
         return (
-            <div className='site-container'> 
+            <div className='site-container'>
+                <section id='top'>
+                    <div className="row">
+                        <div className='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+                            <div className="top-container" >
+                                <div className="cardheader-image">
+                                </div>
+                                <div className="avatar">
+                                    <img src={require('./images/avatar-linda.jpg')} alt={"linda foo profile"}/>
+                                </div>
+                                <div className="top-info">
+                                    <div className="top-name">
+                                        Linda Foo
+                                    </div>
+                                    <div className="top-title">Developer</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <Scroll>{ scrollTop => 
+                        <BoundingRect style={styles.boundingRect}>{rect =>
+                            <div className='navOnTop' style={{...styles.container, ...(rect && rect.top <= 0 && styles.sticky)}}>
+                                <nav className='topNav'>
+                                    <ul className='topNav-list'>
+                                        <li className="qanda">
+                                            <div className='wrapper'>
+                                            <a className="qanda" href="#qanda">
+                                                <span> 
+                                                <img className='icon-qanda icon-bubble' src={require('./images/if_speech_bubble_172556.ico')} alt={"Q&A Icon"}/> 
+                                                Q&A
+                                                </span>
+                                            </a>
+                                            </div>
+                                        </li>
+                                        <li className="portfolio">
+                                        <div className='wrapper'>
+                                            <a className="portfolio" href="#portfolio">
+                                                <span> 
+                                                <img className='icon-portfolio icon-bubble' src={require('./images/290140 - folder.ico')} alt={"Portfolio Icon"}/> 
+                                                PORTFOLIO
+                                                </span>
+                                            </a>
+                                            </div>
+                                        </li>
+                                        <li className="resume">
+                                        <div className='wrapper'>
+                                            <a className="resume" href="#resume">
+                                                <span> 
+                                                <img className='icon-resume icon-bubble' src={require('./images/290138 - file.ico')} alt={"Resume Icon"}/> 
+                                                RESUME
+                                                </span>
+                                            </a>
+                                            </div>
+                                        </li>
+                                        <li className="contact">
+                                        <div className='wrapper'>
+                                        <a className="contact" href="#contact">
+                                                <span> 
+                                                <img className='icon-contact icon-bubble' src={require('./images/if_phone1_172517.ico')} alt={"Contact Icon"}/> 
+                                                CONTACT
+                                                </span>
+                                            </a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </nav>
+                                </div>
+                        }</BoundingRect>
+                    }</Scroll> 
                 <section id='questions'>        
                     <div className='row'>
                         <div className='col-lg-6 col-md-12 col-sm-12 col-xs-12'>
@@ -132,7 +209,62 @@ export default class Home extends React.Component {
                             </div>
                         </div>
                     </div>
-                </section>  
+                </section>
+                <section id='resume'>        
+                    <div className='row'>
+                        <div className='col-lg-6 col-md-12 col-sm-12 col-xs-12'>
+                            <div className="resume-container">
+                                <div className="resume-title">
+                                    Resume
+                                </div>
+                                <div className="resume-file-container">
+                                    <a href="./images/Foo_Linda_Resume_2018.pdf" download="linda-foo-resume.pdf" alt="Download PDF" target="_blank">
+                                        <img className='icon-download' src={require('./images/if_icon-57-document-download_314897.ico')} alt={"Download PDF Icon"}/>
+                                        <span className='instruct-action'>Download PDF</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id='contact'>        
+                    <div className='row'>
+                        <div className='col-lg-6 col-md-12 col-sm-12 col-xs-12'>
+                            <div className="contact-container">
+                                <div className="contact-title">
+                                    Contact
+                                </div>
+                                <div className="contact-detail-container">
+                                    <div className="contact-detail">
+                                        <p className='contact-text'>Let's Connect!</p>
+                                        <p className='contact-type'>Email</p>
+                                        <p><a className='contact-link' target="_top" href="mailto:lindafoodev@gmail.com">lindafoodev@gmail.com</a></p>
+                                        <p className='contact-type'>LinkedIn</p>
+                                        <p><a className='contact-link' href="https://www.linkedin.com/in/linda-foo">linkedin.com/in/linda-foo</a></p>
+                                        <p className='contact-type'>Github</p> 
+                                        <p><a className='contact-link' href="https://github.com/lindafoodev">lindafoodev</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <nav className="bottom-nav">
+                    <div className="bottom-nav-container">
+                        <ul className='bottom-nav-list'>
+                            <li className="return-to-top">
+                                <div className="button-wrapper">
+                                    <a className="bottom-nav-link" href="#top">
+                                        <span> 
+                                        <img className='icon-return' src={require('./images/if_ExpandLess_1031519.ico')} alt={"Return to Top Icon"}/> 
+                                        Return to Top
+                                        </span>
+                                    </a>
+                                </div>
+                            </li>
+                        </ul> 
+                    </div>
+                </nav>  
             </div>  
         );
     }
