62dcb41a45a530d18ab93a14d258ca898475ca31
Linda Foo <linda.foo.dev@gmail.com>
2018-04-02 15:18:37 -0700

    updated text paragraph

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -16,15 +16,15 @@ export default function Portfolio() {
                             <div className="project-title">Zhip</div>
                                 <div className="project-links">
                                     <div className="project-linkBox">
-                                        <a href="https://zhip.netlify.com/home">Demo</a>
+                                        <a href="https://zhip.netlify.com/home" target="_blank">Demo</a>
                                     </div>
                                     <div className="space-container-link"></div>
                                     <div className="project-linkBox">
-                                        <a href="https://github.com/lindafoodev/zhip-app-client">Code</a>
+                                        <a href="https://github.com/lindafoodev/zhip-app-client" target="_blank">Code</a>
                                     </div>
                                 </div>
                             <div className="project-description">
-                                <p className="project-paragraph">Full-stack MERN utility web application to send and receive IOUs between individuals reliably and securely using a generated URL link.</p>
+                                <p className="project-paragraph">Independently developed within a week, a full-stack MERN utility web application to send and receive IOUs between individuals reliably and securely using a generated URL link.</p>
                             </div>
                             <div className="project-subsection-title"> Skills</div>
                             <div className="project-skills">
@@ -37,15 +37,15 @@ export default function Portfolio() {
                             <div className="project-title">Neighborly</div>
                                 <div className="project-links">
                                     <div className="project-linkBox">
-                                        <a href="https://sharingiscaring-app.herokuapp.com/">Demo</a>
+                                        <a href="https://sharingiscaring-app.herokuapp.com/" target="_blank">Demo</a>
                                     </div>
                                     <div className="space-container-link"></div>
                                     <div className="project-linkBox">
-                                        <a href="https://github.com/lindafoodev/Neighborly">Code</a>
+                                        <a href="https://github.com/lindafoodev/Neighborly" target="_blank">Code</a>
                                     </div>
                                 </div>
                             <div className="project-description">
-                                <p className="project-paragraph">Online resource where members of a given community can loan, give away or sell their items all while encouraging interaction to get to know your neighbors.</p>
+                                <p className="project-paragraph">Paired programmed and built an online resource where members of a given community can loan, give away or sell their items all while encouraging interaction to get to know your neighbors.</p>
                             </div>
                             <div className="project-subsection-title"> Skills</div>
                             <div className="project-skills">
