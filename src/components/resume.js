3ef3555756a969b7939613f6eb145b7f24ad46b9
Linda Foo <linda.foo.dev@gmail.com>
2018-04-02 14:13:30 -0700

    move file

--- src/components/resume.js
+++ src/components/resume.js
@@ -15,7 +15,7 @@ export default function Resume() {
                           <ul className="resume-download-list">
                               <li className="download">
                                   <div className='download-wrapper'>
-                                      <a className="resume" href="%PUBLIC_URL%/Foo_Linda_Resume_2018.pdf" download="linda-foo-resume.pdf" alt="Download PDF" target="_blank">
+                                      <a className="resume" href='./assets/Foo_Linda_Resume_2018.pdf' alt="Download PDF" target="_blank">
                                           <span className='icon-text'>
                                           <img className='icon-download' src={require('./images/if_icon-57-document-download_314897.ico')} alt={"Download PDF Icon"}/>
                                           Download PDF</span>
