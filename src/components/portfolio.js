9ef09510614b118296a73fe110169f6b053dd2d0
Linda Foo <linda.foo.dev@gmail.com>
2018-04-20 19:14:44 -0700

    added images

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -11,17 +11,21 @@ export default function Portfolio() {
                     </div>
                     <div className="project-container">
                         <div className="project">
-                            <div className="project-title">Zhip</div>
-                                <div className="project-links">
-                                    <div className="project-linkBox">
-                                        <a href="https://zhip.netlify.com/home" target="_blank">Demo</a>
+                            <div className="left-side">
+                                <img className='screenshot-image' src={require('./images/zhip.png')} alt={"zhip screenshot"}/>
+                            </div>
+                            <div className="right-side">
+                                <div className="project-title">Zhip</div>
+                                    <div className="project-links">
+                                        <div className="project-linkBox">
+                                            <a href="https://zhip.netlify.com/home" target="_blank">Demo</a>
+                                        </div>
+                                        <div className="space-container-link"></div>
+                                        <div className="project-linkBox">
+                                            <a href="https://github.com/lindafoodev/zhip-app-client" target="_blank">Code</a>
+                                        </div>
                                     </div>
-                                    <div className="space-container-link"></div>
-                                    <div className="project-linkBox">
-                                        <a href="https://github.com/lindafoodev/zhip-app-client" target="_blank">Code</a>
-                                    </div>
-                                </div>
-                            <div className="project-description">
+                                <div className="project-description">
                                 <p className="project-paragraph">Independently developed within a week, a full-stack MERN utility web application to send and receive IOUs between individuals reliably and securely using a generated URL link.</p>
                             </div>
                             <div className="project-subsection-title"> Skills</div>
@@ -29,25 +33,31 @@ export default function Portfolio() {
                                 <p className="project-paragraph">Javascript ES6, Nodejs, Express, React/Redux, Mongodb, Mongoose, Heroku, mLab, Travis CI, Github</p>
                             </div>
                         </div>
+                        </div>
                     </div>
                     <div className="project-container">
                         <div className="project">
-                            <div className="project-title">Centsible.</div>
-                                <div className="project-links">
-                                    <div className="project-linkBox">
-                                        <a href="https://centsible.netlify.com/" target="_blank">Demo</a>
-                                    </div>
-                                    <div className="space-container-link"></div>
-                                    <div className="project-linkBox">
-                                        <a href="https://github.com/thinkful-ei17/teamThree-client" target="_blank">Code</a>
-                                    </div>
-                                </div>
-                            <div className="project-description">
-                                <p className="project-paragraph">Collaboratively built an investment training application to help new investors start making cents of investing, performed as project manager for the 5-person team.</p>
+                            <div className="left-side">
+                                <img className='screenshot-image' src={require('./images/centsible.png')} alt={"centsible screenshot"}/>
                             </div>
-                            <div className="project-subsection-title"> Skills</div>
-                            <div className="project-skills">
-                                <p className="project-paragraph">Javascript ES6, React/Redux, Nodejs, Express, Bcryptjs, jsonwebtoken, Passport, Mongodb, Mongoose, Heroku, mLab, Travis CI, Netlify, Github, Mocha, Chai</p>
+                            <div className="right-side">
+                                <div className="project-title">Centsible.</div>
+                                    <div className="project-links">
+                                        <div className="project-linkBox">
+                                            <a href="https://centsible.netlify.com/" target="_blank">Demo</a>
+                                        </div>
+                                        <div className="space-container-link"></div>
+                                        <div className="project-linkBox">
+                                            <a href="https://github.com/thinkful-ei17/teamThree-client" target="_blank">Code</a>
+                                        </div>
+                                    </div>
+                                <div className="project-description">
+                                    <p className="project-paragraph">Collaboratively built an investment training application to help new investors start making cents of investing, performed as project manager for the 5-person team.</p>
+                                </div>
+                                <div className="project-subsection-title"> Skills</div>
+                                <div className="project-skills">
+                                    <p className="project-paragraph">Javascript ES6, React/Redux, Nodejs, Express, Bcryptjs, jsonwebtoken, Passport, Mongodb, Mongoose, Heroku, mLab, Travis CI, Netlify, Github, Mocha, Chai</p>
+                                </div>
                             </div>
                         </div>
                     </div>
