f7af4e1076c9b7da6f3a9378e20a7041fbfbe907
Linda Foo <linda.foo.dev@gmail.com>
2018-07-09 23:15:15 -0700

    added resume abbreviated details

--- src/components/resume.js
+++ src/components/resume.js
@@ -18,26 +18,42 @@ export default function Resume() {
                         </a>
                     </div>
                     <div>
-                    <table className='MsoNormal'>
- <tr className='table-row'>
-  <td className='table-cell'>
-  <p className='table-paragraph'>SKILLS
-  &amp; TOOLS</p>
-  </td>
- </tr>
- <tr className='table-row'>
-  <td className='table-cell'>
-  <p className='table-paragraph'><span>Frontend:</span><span> JavaScript ES6, HTML5,
-  CSS3, jQuery, AJAX/JSON, React, Redux, Webpack</span></p>
-  </td>
- </tr>
- <tr>
-  <td className='table-cell'>
-    <h2>EXPERIENCE</h2>
-  </td>
- </tr></table>
-</div>
-
+                        <table className='resume-table'>
+                            <tr className='table-row'>
+                                <td className='table-cell table-section-title italic-text bold-text'>SKILLS &amp; TOOLS</td>
+                            </tr>
+                            <tr className='table-row'>
+                                <td className='table-cell table-paragraph'><span className='bold-text'>Frontend:</span><span> JavaScript ES6, HTML5, CSS3, jQuery, AJAX/JSON, React, Redux, Webpack</span></td>
+                            </tr>
+                            <tr className='table-row'>
+                                <td className='table-cell table-paragraph'><span className='bold-text'>Backend:</span><span> Node.js, Express.js, RESTful API, NoSQL (MongoDB), SQL (PostgreSQL), JWT (Passport)</span></td>
+                            </tr>
+                            <tr className='table-row'>
+                                <td className='table-cell table-paragraph'><span className='bold-text'>Development Environment:</span><span> VSC, Chrome Developer Tools, Git, GitHub, Travis CI, Heroku, Netlify, npm, mLab</span></td>
+                            </tr>
+                            <tr className='table-row'>
+                                <td className='table-cell table-section-title italic-text bold-text'>EXPERIENCE</td>
+                            </tr>
+                            <tr className='table-row'>
+                                <td className='table-cell table-paragraph'><span className='bold-text'>JPMorgan Chase & Co.</span></td>
+                            </tr>
+                            <tr className='table-row'>
+                                <td className='table-cell table-paragraph'><span className='italic-text'>Assistant Vice President - </span><span>Performed operational reviews, specializing in chargeback management & prevention and merchant payment processing services ($1 trillion in volume).</span></td>
+                            </tr>
+                            <tr className='table-row'>
+                                <td className='table-cell table-paragraph'><span className='bold-text'>PricewaterhouseCoopers LLP (PwC)</span></td>
+                            </tr>
+                            <tr className='table-row'>
+                                <td className='table-cell table-paragraph'><span className='italic-text'>Assurance Senior Associate - </span><span>Supervised 9 staff and interns on financial audits. Leveraged interpersonal skills to build strong professional relationships with client management and staff. Received two recognition awards for outstanding performance.</span></td>
+                            </tr>
+                            <tr className='table-row'>
+                                <td className='table-cell table-paragraph'><span className='bold-text'>Grant Thornton LLP (PwC)</span></td>
+                            </tr>
+                            <tr className='table-row'>
+                                <td className='table-cell table-paragraph'><span className='italic-text'>Senior Audit Associate - </span><span>Executed financial statement reviews for public and private companies with revenues from $5 million to $2 billion in various industries, including Financial Services, Manufacturing, Aerospace Restaurant, and Oil & Gas, touching all aspects of business, such as Revenue and Expenses, Cash, Intangibles, Inventory, Fixed Assets, Debt, and Equity.</span></td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
