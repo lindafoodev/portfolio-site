8d9b9c8416c8e8bf7e694d2952b289b787b4a9aa
Linda Foo <linda.foo.dev@gmail.com>
2018-07-09 22:13:08 -0700

    edited text, reformatting resume section

--- src/components/question-and-answer.js
+++ src/components/question-and-answer.js
@@ -13,7 +13,7 @@ export default function QuestionAndAnswer() {
                         Can you tell me about yourself?
                     </div>
                     <div className="questions-content">
-                        <p className="questions-paragraph">I am a developer with domain knowledge in business. I am known for my ability to effectively decipher information and efficiently utilize resources available to me to perform. I have a proven track record of continuously taking on challenges to further my personal and professional development.</p>
+                        <p className="questions-paragraph">I am a developer with domain knowledge in finance (CPA). I am known for my ability to effectively decipher information and efficiently utilize resources available to me to perform. I have a proven track record of continuously taking on challenges to further my personal and professional development.</p>
                         <p className="questions-paragraph">I graduated from The University of Texas at Austin with a Bachelor of Business Administration and a Master in Professional Accounting. I am a licensed CPA. My professional experiences include managing operational reviews for JPMorgan Chase & Co. and external financial audits for PricewaterhouseCoopers LLP (PwC) and Grant Thornton LLP. I have worked on a variety of projects in various industries including Financial Services, Manufacturing, Aerospace, Restaurant, and Oil & Gas.</p>
                     </div>
                 </div>
--- src/components/resume.js
+++ src/components/resume.js
@@ -2,29 +2,45 @@ import React from 'react';
 
 export default function Resume() {
     return (
-      <section id='resume'>        
-          <div className='section'>
-              <div className='allocate-container'>
-                  <div className="resume-container">
-                      <div className="resume-title">
-                          Resume
-                      </div>
-                      <div className="resume-file-container">
-                          <ul className="resume-download-list">
-                              <li className="download">
-                                  <div className='download-wrapper'>
-                                      <a className="resume" href='./assets/Foo_Linda_Resume_2018_pub.pdf' alt="Download Resume" target="_blank">
-                                          <span className='icon-text'>
-                                          <img className='icon-download' src={require('./images/if_icon-70-document-file-pdf_315672.ico')} alt={"Download PDF Icon"}/>
-                                          Download Resume</span>
-                                      </a>
-                                  </div>
-                              </li>
-                          </ul>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </section>
+    <section id='resume'>        
+        <div className='section'>
+            <div className="resume-container">
+                <div className="resume-title">
+                    Resume
+                </div>
+                <div className="resume-container">
+                    <div className='wrapper-resume'>
+                        <a href='./assets/Foo_Linda_Resume_2018_pub.pdf' alt="Download Resume" target="_blank">
+                        <span>
+                            <img className='link-bubble' src={require('./images/if_icon-70-document-file-pdf_315672.ico')} alt={"Download PDF Icon"}/>
+                            <span className='details'>Download</span>
+                        </span>
+                        </a>
+                    </div>
+                    <div>
+                    <table className='MsoNormal'>
+ <tr className='table-row'>
+  <td className='table-cell'>
+  <p className='table-paragraph'>SKILLS
+  &amp; TOOLS</p>
+  </td>
+ </tr>
+ <tr className='table-row'>
+  <td className='table-cell'>
+  <p className='table-paragraph'><span>Frontend:</span><span> JavaScript ES6, HTML5,
+  CSS3, jQuery, AJAX/JSON, React, Redux, Webpack</span></p>
+  </td>
+ </tr>
+ <tr>
+  <td className='table-cell'>
+    <h2>EXPERIENCE</h2>
+  </td>
+ </tr></table>
+</div>
+
+                </div>
+            </div>
+        </div>
+    </section>
     );
 }
